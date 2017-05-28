14509d307153e5e699052b231ed1c5ad9bad1449
zhouyang <zhouyang@shanghaitech.edu.cn>
2017-05-28 08:24:59 +0800

    rm target

--- target/debug/hey-stop.d
+++
@@ -1,1 +0,0 @@
-/home/makee/github/hey-stop/target/debug/hey-stop: /home/makee/github/hey-stop/src/main.rs /home/makee/github/hey-stop/src/lib.rs
--- target/debug/libhey_stop.d
+++
@@ -1,1 +0,0 @@
-/home/makee/github/hey-stop/target/debug/libhey_stop.rlib: /home/makee/github/hey-stop/src/lib.rs
